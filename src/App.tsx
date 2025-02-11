c9e2f9a9cd5ab121ff7f54e28e879d6cfd3bff42
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-02-11 13:28:23 +0700

    Demo 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import "./App.css";
 // import { ProtectedRoute } from './components/ProtectedRoute'
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
+import MainLayout from "./layouts/MainLayout";
+import Dashboard from "./pages/admin/Dashboard";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/adminDashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       {/* <ProtectedRoute>
         
