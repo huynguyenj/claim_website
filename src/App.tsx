65884a594195b013156deebd0171f48d5f5ae408
giakien321 <kientgse182386@fpt.edu.vn>
2025-02-13 01:48:42 +0700

    Request Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
 import {Route, Routes } from 'react-router-dom'
 import './App.css'
 // import { ProtectedRoute } from './components/ProtectedRoute'
 import LoginPage from './pages/auth/LoginPage'
 import RegisterPage from './pages/auth/RegisterPage'
 import MainLayout from './layouts/MainLayout'
+import RequestPage from './pages/user/RequestPage'
+
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path='/' element={<MainLayout/>}>
+                <Route path="requests" element={<RequestPage />} />
                 {/* All pages in here route write in here*/}
             </Route>
     </Routes>
