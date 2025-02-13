d3c78359e08c24b8b76b6b762d35f9f5baf22c71
DanieLsvenn <seizurebabe@gmail.com>
2025-02-13 13:19:44 +0700

    format

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,31 @@ import { Suspense } from "react";
 import ListPublicRoute from "./router/PublicRoute";
 import PrivateRoute from "./router/PrivateRoute";
 import Loading from "./components/Loading";
+import { AdminRoutes } from "./router/AdminRoutes";
+import Project from "./components/Project"
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <>
-      <Suspense fallback={<Loading />}>
+      {/* <Suspense fallback={<Loading />}>
         <Routes>
-          {/* Public route for all user */}
           {ListPublicRoute.map((route, index) => (
             <Route key={index} path={route.path} element={route.element} />
           ))}
 
           <Route element={<ProtectedRoute />}>
-            {/* Using /* to match any path after / but you need to sovle '*' to make sure it not have any issues when you navigate */}
             <Route path="/*" element={<PrivateRoute />}></Route>
           </Route>
 
         </Routes>
-      </Suspense>
+      </Suspense> */}
+
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path={AdminRoutes.PROJECT_DASHBOARD} element={<Project />} />
+        </Route>
+      </Routes>
 
     </>
   );
