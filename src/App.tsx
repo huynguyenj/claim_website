eb0dc2dd7269a647d694ce042bd906e54226b951
anhvu <nguyentrananhvu04@gmail.com>
2025-02-13 10:26:26 +0700

    work/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { ProtectedRoute } from "./router/ProtectedRoute";
 import { Suspense } from "react";
 import ListPublicRoute from "./router/PublicRoute";
 import PrivateRoute from "./router/PrivateRoute";
 import Loading from "./components/Loading";
-import MainLayout from "./layouts/MainLayout";
-import ApprovalPage from "./pages/auth/ApprovalPage";
-import { UserRoutes } from "./router/UserRoutes";
 
 function App() {
   return (
     <>
-       {/* <Suspense fallback={<Loading/>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
-        {ListPublicRoute.map((route,index)=>(
-          <Route key={index} path={route.path} element={route.element}/>
-        ))}
+          {/* Public route for all user */}
+          {ListPublicRoute.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
 
-        <Route element={<ProtectedRoute/>}>
-            <Route path="/*" element={<PrivateRoute/>}></Route> 
-        </Route>
-      
-      </Routes>
-      </Suspense> */}
-    <Routes>
-      <Route element={<MainLayout/>}>
-        <Route path={UserRoutes.UserApproval} element={<ApprovalPage/>}></Route>
-      </Route>
-    </Routes>
+          <Route element={<ProtectedRoute />}>
+            {/* Using /* to match any path after / but you need to sovle '*' to make sure it not have any issues when you navigate */}
+            <Route path="/*" element={<PrivateRoute />}></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
