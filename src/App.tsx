ea810ae74f526c14514af044869551f6b7334b82
Nguyendat <datntse170123@fpt.edu.vn>
2025-02-13 10:57:31 +0700

    UserDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/admin/Dashboard";
+import UserDashboard from "./pages/user/UseDashboard";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<MainLayout />}>
           <Route path="/adminDashboard" element={<Dashboard />} />
+          <Route path="/userDashboard" element={<UserDashboard />} />
         </Route>
       </Routes>
       {/* <ProtectedRoute>
