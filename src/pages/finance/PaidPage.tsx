8f1a8a4ce42be882f8b6895bd918258983556829
Tantai231204 <taintse184659@fpt.edu.vn>
2025-02-14 17:57:42 +0700

    create 70%

--- src/pages/finance/PaidPage.tsx
+++ src/pages/finance/PaidPage.tsx
@@ -1,14 +1,50 @@
 import { Col, Row } from "antd";
 import SalaryTable from "./SalaryTable";
-
+import Sidebar from "../../layouts/Sidebar";
+import { SidebarItem } from "../../data/SidebarData";
+import {
+  DashBoard,
+  LogoutIcon,
+  PersonIcon,
+  SettingsIcon,
+} from "../../components/MuiIIcon";
+import Navbar from "../../layouts/Navbar";
 
 const PaidPage: React.FC = () => {
+  const sidebarItems: SidebarItem[] = [
+    {
+      title: "Dashboard",
+      icon: DashBoard,
+      path: "/dashboard",
+      role: "admin",
+    },
+    {
+      title: "Profile",
+      icon: PersonIcon,
+      path: "/profile",
+      role: "user",
+    },
+    {
+      title: "Settings",
+      icon: SettingsIcon,
+      path: "/settings",
+      role: "admin",
+      gap: true,
+    },
+    {
+      title: "Logout",
+      icon: LogoutIcon,
+      action: () => console.log("User logged out"),
+    },
+  ];
 
   return (
     <div>
       <Row className="h-screen">
+        <Navbar />
         <Col sm={5} md={4} lg={3} className="h-full">
-        </Col>  
+          <Sidebar itemList={sidebarItems} />
+        </Col>
         <Col sm={19} md={20} lg={21} className="w-full p-4">
           <SalaryTable />
         </Col>
