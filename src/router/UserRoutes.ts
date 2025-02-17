020b8eab2405be7aabb75b1ed263806f59e19f35
giakien321 <kientgse182386@fpt.edu.vn>
2025-02-17 16:11:01 +0700

    update

--- src/router/UserRoutes.ts
+++ src/router/UserRoutes.ts
@@ -1,7 +1,5 @@
 export const UserRoutes = {
       USER_DASHBOARD: '/userdashboard',
-<<<<<<< HEAD
-=======
-      UserApproval:'/userapproval'
->>>>>>> 9577b364b4cab47ac9c1ee7fdc467ce130287e19
+      UserApproval:'/userapproval',
+      
 }
