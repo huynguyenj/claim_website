216d1dc61c10ea75ba346ac0736edfaf41232ed3
giakien321 <kientgse182386@fpt.edu.vn>
2025-02-17 21:37:21 +0700

    Delete UserRoutes in router folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import { Route, Routes } from "react-router-dom";
-import { ProtectedRoute } from "./router/ProtectedRoute";
-import { Suspense } from "react";
-import ListPublicRoute from "./router/PublicRoute";
-import PrivateRoute from "./router/PrivateRoute";
-import Loading from "./components/Loading";
+import RequestPage from "./pages/user/RequestPage";
+import MainLayout from "./layouts/MainLayout";
+import { UserRoutes } from "./consts/RoutesConst";
+// import { ProtectedRoute } from "./router/ProtectedRoute";
+// import { Suspense } from "react";
+// import ListPublicRoute from "./router/PublicRoute";
+// import PrivateRoute from "./router/PrivateRoute";
+// import Loading from "./components/Loading";
 
 
 function App() {
   return (
     <>
-       <Suspense fallback={<Loading/>}>
+       {/* <Suspense fallback={<Loading/>}>
         <Routes>
-        {/* Public route for all user */}
+        
         {ListPublicRoute.map((route,index)=>(
           <Route key={index} path={route.path} element={route.element}/>
         ))}
 
         <Route element={<ProtectedRoute/>}>
-            {/* Using /* to match any path after / but you need to sovle '*' to make sure it not have any issues when you navigate */}
+            
             <Route path="/*" element={<PrivateRoute/>}></Route> 
         </Route>
       
       </Routes>
-      </Suspense>
+      </Suspense> */}
+       <Routes>
+        <Route element={<MainLayout />}>
+          <Route path={UserRoutes.REQUEST_PAGE} element={<RequestPage />} />
+        </Route>
+      </Routes>
     
     </>
   );
--- src/router/UserRoutes.ts
+++
@@ -1,5 +0,0 @@
-export const UserRoutes = {
-      USER_DASHBOARD: '/userdashboard',
-      UserApproval:'/userapproval',
-      
-}
