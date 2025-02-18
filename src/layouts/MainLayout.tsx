d695c54763359060944fde2279db568c129ae791
huynguyenj <nguyenhuyjobs@gmail.com>
2025-02-18 08:52:19 +0700

    updata/file/nguyenlegiahuy

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,25 +3,23 @@ import Sidebar from "./Sidebar";
 import { useEffect, useState } from "react";
 import { SidebarItem } from "../model/SidebarData";
 import { useAuthStore } from "../store/authStore";
-import { ApprovalIcon, CheckListIcon, DashBoard, PaidIcon, RequestPageIcon, SettingIcon, UserList, UserProfile } from "../components/Icon/MuiIIcon";
+import { ApprovalIcon, CheckListIcon, DashBoard, PaidIcon, RequestPageIcon, UserList, UserProfile } from "../components/Icon/MuiIIcon";
 import Navbar from "./Navbar";
 import { UserRoutes, AdminRoutes } from "../consts/RoutesConst";
 import { roleDefine } from "../consts/UserRole";
 
 const sideBarUser: SidebarItem[] = [
-  { title: 'Claim data', icon: DashBoard, path: '/userDashboard' },
+  { title: 'Claim data', icon: DashBoard, path: UserRoutes.USER_DASHBOARD },
   { title: 'Request', icon: RequestPageIcon, path: UserRoutes.REQUEST_PAGE },
   { title: 'Approval', icon: ApprovalIcon, path: UserRoutes.APPROVAL_PAGE, role: roleDefine.APPROVAL_ROLE, gap: true },
   { title: 'Profile', icon: UserProfile, path: '/userprofile' },
   { title: 'Paid', icon: PaidIcon, path: '/paidPage', role: roleDefine.FINANCE },
-  { title: 'Setting', icon: SettingIcon, path: '/setting', gap: true },
 ]
 
 const sideBarAdmin: SidebarItem[] = [
   { title: 'Dashboard', icon: DashBoard, path: AdminRoutes.ADMIN_DASHBOARD, gap: true },
   { title: 'User Management', icon: UserList, path: AdminRoutes.USER_LIST_PAGE },
   { title: 'Project Management', icon: CheckListIcon, path: AdminRoutes.PROJECT_LIST_PAGE, gap: true },
-  { title: 'Setting', icon: SettingIcon, path: '/setting' },
 
 ]
 
