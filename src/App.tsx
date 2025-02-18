38397b8c5c35f52b409a25ee6206d7b7b63aeea8
DanieLsvenn <seizurebabe@gmail.com>
2025-02-18 08:07:30 +0700

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import { ProtectedRoute } from "./router/ProtectedRoute";
 import { Suspense } from "react";
-import ListPublicRoute from "./router/PublicRoute";
-import PrivateRoute from "./router/PrivateRoute";
+import { Route, Routes } from "react-router-dom";
 import Loading from "./components/Loading";
-import { AdminRoutes } from "./router/AdminRoutes";
-import Project from "./components/Project"
-import MainLayout from "./layouts/MainLayout";
+import PrivateRoute from "./router/PrivateRoute";
+import { ProtectedRoute } from "./router/ProtectedRoute";
+import ListPublicRoute from "./router/PublicRoute";
 
 
 function App() {
   return (
     <>
-      {/* <Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           {ListPublicRoute.map((route, index) => (
             <Route key={index} path={route.path} element={route.element} />
@@ -23,14 +20,7 @@ function App() {
           </Route>
 
         </Routes>
-      </Suspense> */}
-
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path={AdminRoutes.PROJECT_DASHBOARD} element={<Project />} />
-        </Route>
-      </Routes>
-
+      </Suspense>
     </>
   );
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,44 +3,25 @@ import Sidebar from "./Sidebar";
 import { useEffect, useState } from "react";
 import { SidebarItem } from "../model/SidebarData";
 import { useAuthStore } from "../store/authStore";
-import { ApprovalIcon, CheckListIcon, DashBoard,PaidIcon, RequestPageIcon, SettingIcon, UserList, UserProfile } from "../components/Icon/MuiIIcon";
+import { ApprovalIcon, CheckListIcon, DashBoard, PaidIcon, RequestPageIcon, SettingIcon, UserList, UserProfile } from "../components/Icon/MuiIIcon";
 import Navbar from "./Navbar";
-import { UserRoutes,AdminRoutes } from "../consts/RoutesConst";
+import { UserRoutes, AdminRoutes } from "../consts/RoutesConst";
 import { roleDefine } from "../consts/UserRole";
 
-<<<<<<< HEAD
 const sideBarUser: SidebarItem[] = [
   { title: 'Claim data', icon: DashBoard, path: '/userDashboard' },
-  { title: 'Request', icon: RequestPageIcon, path: '/requestPage' },
-  { title: 'Approval', icon: ApprovalIcon, path: '/approvalPage', role: 'BA', gap: true },
+  { title: 'Request', icon: RequestPageIcon, path: UserRoutes.REQUEST_PAGE },
+  { title: 'Approval', icon: ApprovalIcon, path: UserRoutes.APPROVAL_PAGE, role: roleDefine.APPROVAL_ROLE, gap: true },
   { title: 'Profile', icon: UserProfile, path: '/userprofile' },
-  { title: 'Paid', icon: PaidIcon, path: '/paidPage', role: "finance" },
+  { title: 'Paid', icon: PaidIcon, path: '/paidPage', role: roleDefine.FINANCE },
   { title: 'Setting', icon: SettingIcon, path: '/setting', gap: true },
-  { title: 'Logout', icon: LogoutIcon, action: () => useAuthStore.getState().removeExpired() },
 ]
 
 const sideBarAdmin: SidebarItem[] = [
-  { title: 'Dashboard', icon: DashBoard, path: '/adminDashboard', gap: true },
-  { title: 'User Management', icon: UserList, path: '/userlist' },
-  { title: 'Project Manager', icon: CheckListIcon, path: '/projectManage', gap: true },
+  { title: 'Dashboard', icon: DashBoard, path: AdminRoutes.ADMIN_DASHBOARD, gap: true },
+  { title: 'User Management', icon: UserList, path: AdminRoutes.USER_LIST_PAGE },
+  { title: 'Project Management', icon: CheckListIcon, path: AdminRoutes.PROJECT_LIST_PAGE, gap: true },
   { title: 'Setting', icon: SettingIcon, path: '/setting' },
-  { title: 'Logout', icon: LogoutIcon, action: () => useAuthStore.getState().removeExpired() },
-=======
-const sideBarUser:SidebarItem[]=[
-  {title:'Claim data',icon:DashBoard,path:'/userDashboard'},
-  {title:'Request',icon:RequestPageIcon, path:UserRoutes.REQUEST_PAGE},
-  {title:'Approval',icon:ApprovalIcon, path:UserRoutes.APPROVAL_PAGE, role:roleDefine.APPROVAL_ROLE, gap:true},
-  {title:'Profile',icon:UserProfile,path:'/userprofile'},
-  {title:'Paid',icon:PaidIcon,path:'/paidPage', role:roleDefine.FINANCE},
-  {title:'Setting',icon:SettingIcon,path:'/setting',gap:true},
-]
-
-const sideBarAdmin:SidebarItem[]=[
-  {title:'Dashboard',icon:DashBoard,path:AdminRoutes.ADMIN_DASHBOARD, gap:true},
-  {title:'User Management',icon:UserList,path:AdminRoutes.USER_LIST_PAGE},
-  {title:'Project Management',icon:CheckListIcon,path:AdminRoutes.PROJECT_LIST_PAGE, gap:true},
-  {title:'Setting',icon:SettingIcon,path:'/setting'},
->>>>>>> e287a514e61aae452d4b1e53f9928fd2812abffb
 
 ]
 
@@ -54,27 +35,14 @@ export default function MainLayout() {
   }, [userRole])
   return (
     <>
-<<<<<<< HEAD
       <div className="flex h-screen overflow-hidden">
-        {userRole == "admin" ? <Sidebar itemList={sideBarAdmin} /> : <Sidebar itemList={item} />}
+        {userRole == roleDefine.ADMIN_ROLE ? <Sidebar itemList={sideBarAdmin} /> : <Sidebar itemList={item} />}
 
         <main className="flex-1 flex flex-col">
           <Navbar />
-          <Outlet>
-
-          </Outlet>
+          <Outlet />
         </main>
       </div>
-=======
-    <div className="flex h-screen overflow-hidden">
-      {userRole == roleDefine.ADMIN_ROLE ? <Sidebar itemList={sideBarAdmin}/>:<Sidebar itemList={item}/>}
-  
-      <main className="flex-1 flex flex-col">
-            <Navbar/>
-            <Outlet/>              
-      </main>
-    </div>
->>>>>>> e287a514e61aae452d4b1e53f9928fd2812abffb
     </>
   )
 }
