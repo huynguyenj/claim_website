94e521d701a82b538d1220cc00b4715313428456
DESKTOP-JHGK7K2\HP <taintse184659@fpt.edu.vn>
2025-02-18 09:36:23 +0700

    update

--- src/consts/RoutesConst.ts
+++ src/consts/RoutesConst.ts
@@ -1,22 +1,19 @@
 export const PublicRoutes = {
-  HOME: "/",
-  LOGIN: "/login",
-  REGISTER: "/register",
-  FINANCE: "/paidPage",
-};
+      HOME: '/',
+      LOGIN:'/login',
+      REGISTER:'/register',
+ }
 
-export const AdminRoutes = {
-  ADMIN_DASHBOARD: "/adminDashboard",
-  PROJECT_LIST_PAGE: "/projectList",
-  USER_LIST_PAGE: "/userList",
-};
-export const FinanceRoutes = {
-  FINANCE: "/paidPage",
-};
+ export const AdminRoutes = {
+      ADMIN_DASHBOARD: '/adminDashboard',
+      PROJECT_LIST_PAGE: '/projectList',
+      USER_LIST_PAGE: '/userList'
+}
 
 export const UserRoutes = {
-  USER_DASHBOARD: "/userdashboard",
-  REQUEST_PAGE: "/request",
-  APPROVAL_PAGE: "/approvalPage",
-  PROFILE_PAGE: "/userprofile",
-};
+      USER_DASHBOARD: '/userdashboard',
+      REQUEST_PAGE: '/request',
+      APPROVAL_PAGE: '/approvalPage',
+      PROFILE_PAGE: '/userprofile',
+      PAID_PAGE : '/paidPage'
+}
