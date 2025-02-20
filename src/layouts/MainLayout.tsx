b063653497a0586139f395537447ae0e15931f1f
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-02-20 15:21:09 +0700

    Admin Dashboard - new layout
    Remake new layout from figma

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,46 +3,86 @@ import Sidebar from "./Sidebar";
 import { useEffect, useState } from "react";
 import { SidebarItem } from "../model/SidebarData";
 import { useAuthStore } from "../store/authStore";
-import { ApprovalIcon, CheckListIcon, DashBoard,PaidIcon, RequestPageIcon, SettingIcon, UserList, UserProfile } from "../components/Icon/MuiIIcon";
+import {
+  ApprovalIcon,
+  CheckListIcon,
+  DashBoard,
+  PaidIcon,
+  RequestPageIcon,
+  SettingIcon,
+  UserList,
+  UserProfile,
+} from "../components/Icon/MuiIIcon";
 import Navbar from "./Navbar";
-import { UserRoutes,AdminRoutes } from "../consts/RoutesConst";
+import { UserRoutes, AdminRoutes } from "../consts/RoutesConst";
 import { roleDefine } from "../consts/UserRole";
 
-const sideBarUser:SidebarItem[]=[
-  {title:'Claim data',icon:DashBoard,path:'/userDashboard'},
-  {title:'Request',icon:RequestPageIcon, path:UserRoutes.REQUEST_PAGE},
-  {title:'Approval',icon:ApprovalIcon, path:UserRoutes.APPROVAL_PAGE, role:roleDefine.APPROVAL_ROLE, gap:true},
-  {title:'Profile',icon:UserProfile,path:'/userprofile'},
-  {title:'Paid',icon:PaidIcon,path:'/paidPage', role:roleDefine.FINANCE},
-  {title:'Setting',icon:SettingIcon,path:'/setting',gap:true},
-]
+const sideBarUser: SidebarItem[] = [
+  { title: "Claim data", icon: DashBoard, path: "/userDashboard" },
+  { title: "Request", icon: RequestPageIcon, path: UserRoutes.REQUEST_PAGE },
+  {
+    title: "Approval",
+    icon: ApprovalIcon,
+    path: UserRoutes.APPROVAL_PAGE,
+    role: roleDefine.APPROVAL_ROLE,
+    gap: true,
+  },
+  { title: "Profile", icon: UserProfile, path: "/userprofile" },
+  {
+    title: "Paid",
+    icon: PaidIcon,
+    path: "/paidPage",
+    role: roleDefine.FINANCE,
+  },
+  { title: "Setting", icon: SettingIcon, path: "/setting", gap: true },
+];
 
-const sideBarAdmin:SidebarItem[]=[
-  {title:'Dashboard',icon:DashBoard,path:AdminRoutes.ADMIN_DASHBOARD, gap:true},
-  {title:'User Management',icon:UserList,path:AdminRoutes.USER_LIST_PAGE},
-  {title:'Project Management',icon:CheckListIcon,path:AdminRoutes.PROJECT_LIST_PAGE, gap:true},
-  {title:'Setting',icon:SettingIcon,path:'/setting'},
-
-]
-
+const sideBarAdmin: SidebarItem[] = [
+  {
+    title: "Dashboard",
+    icon: DashBoard,
+    path: AdminRoutes.ADMIN_DASHBOARD,
+    gap: true,
+  },
+  {
+    title: "User Management",
+    icon: UserList,
+    path: AdminRoutes.USER_LIST_PAGE,
+  },
+  {
+    title: "Project Management",
+    icon: CheckListIcon,
+    path: AdminRoutes.PROJECT_LIST_PAGE,
+    gap: true,
+  },
+  { title: "Setting", icon: SettingIcon, path: "/setting" },
+];
 
 export default function MainLayout() {
-  const [item,setItem] = useState<SidebarItem[]>([])
+  const [item, setItem] = useState<SidebarItem[]>([]);
   const userRole = useAuthStore.getState().role;
-  useEffect(()=>{
-    console.log(userRole)
-    setItem(sideBarUser.filter((item)=>item.role === userRole || item.role === undefined));
-  },[userRole])
+  useEffect(() => {
+    console.log(userRole);
+    setItem(
+      sideBarUser.filter(
+        (item) => item.role === userRole || item.role === undefined
+      )
+    );
+  }, [userRole]);
   return (
     <>
-    <div className="flex h-screen overflow-hidden">
-      {userRole == roleDefine.ADMIN_ROLE ? <Sidebar itemList={sideBarAdmin}/>:<Sidebar itemList={item}/>}
-  
-      <main className="flex-1 flex flex-col">
-            <Navbar/>
-            <Outlet/>              
-      </main>
-    </div>
+      <div className="flex h-screen overflow-hidden">
+        {userRole == roleDefine.ADMIN_ROLE ? (
+          <Sidebar itemList={sideBarAdmin} />
+        ) : (
+          <Sidebar itemList={item} />
+        )}
+
+        <main className="flex-1 flex flex-col">
+          <Navbar />
+          <Outlet />
+        </main>
+      </div>
     </>
-  )
+  );
 }
