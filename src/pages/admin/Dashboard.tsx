a39e16b7b97d5ed8517746ac43191889cca9026a
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-03-17 20:17:19 +0700

    Admin Dashboard - Features & Layouts
    added the sort for "All", "Users", "Claims", and "Projects". Fixed some layout and responsive issues.

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -13,37 +13,39 @@ export default function Dashboard() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 px-5 md:px-14 overflow-auto pb-5">
       {/* Search Section */}
-      <div className="col-span-1 sm:col-span-2 lg:col-span-4 flex flex-col lg:flex-row items-center justify-between">
+      <div className="col-span-1 sm:col-span-2 lg:col-span-4 flex flex-col lg:flex-row">
         <h1 className="text-3xl font-bold">Dashboard</h1>
       </div>
       {/* Statistics*/}
-      {/* Users */}
-      <StatisticCard
-        icon={<UserIcon />}
-        title="Users"
-        data={totalUsers}
-        loading={userLoading}
-      />
-      {/* Claims */}
-      <StatisticCard
-        icon={<Article />}
-        title="Claims"
-        data={totalClaims}
-        loading={claimLoading}
-      />
-      {/* Projects */}
-      <StatisticCard
-        icon={<Article />}
-        title="Projects"
-        data={totalProjects}
-        loading={projectLoading}
-      />
+      <div className="flex flex-wrap sm:flex-nowrap gap-5 col-span-1 sm:col-span-2 lg:col-span-4">
+        {/* Users */}
+        <StatisticCard
+          icon={<UserIcon />}
+          title="Users"
+          data={totalUsers}
+          loading={userLoading}
+        />
+        {/* Claims */}
+        <StatisticCard
+          icon={<Article />}
+          title="Claims"
+          data={totalClaims}
+          loading={claimLoading}
+        />
+        {/* Projects */}
+        <StatisticCard
+          icon={<Article />}
+          title="Projects"
+          data={totalProjects}
+          loading={projectLoading}
+        />
+      </div>
       {/* Charts Container */}
       <div className="col-span-1 sm:col-span-2 lg:col-span-4 grid grid-cols-1 lg:grid-cols-7 gap-4 lg:gap-0">
         {/* StackedAreaChart */}
         <div
           className="relative h-[28rem] sm:h-[34rem]
-       col-span-1 sm:col-span-2 lg:col-span-full lg:row-span-2 bg-white border pl-5 pt-5 pb-7 rounded-xl"
+       col-span-1 sm:col-span-2 lg:col-span-full lg:row-span-2 bg-white border pl-5 pt-5 pb-15 sm:pb-7 rounded-xl"
         >
           <StackedAreaChart />
         </div>
